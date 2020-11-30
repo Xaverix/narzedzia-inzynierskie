7aa0a213bb688cea6375d19dfaa31b4c3971768e
Xaverix <minesoft@wp.pl>
2020-11-30 21:05:10 +0100

    Added helper for handling InputFields

---
+++ TMProInputFieldHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TMProInputFieldHelper : MonoBehaviour
+{
+    public void MoveToTextStart() {
+        //GetComponent<TMP_InputField>().textComponent.rectTransform.anchoredPosition = Vector2.zero;
+        //GetComponent<TMP_InputField>().stringPosition = 0;
+        //GetComponent<TMP_InputField>().caretPosition = 0;
+        //GetComponent<TMP_InputField>().ForceLabelUpdate();
+        StartCoroutine(MoveToTextStartDelayed());
+    }
+    
+    IEnumerator MoveToTextStartDelayed() {
+        yield return new WaitForEndOfFrame();
+        //GetComponent<TMP_InputField>().Select();
+        GetComponent<TMP_InputField>().MoveToStartOfLine(false, false);
+        //GetComponent<TMP_InputField>().ForceLabelUpdate();
+    }
+
+    public void MoveToTextEnd() {
+        GetComponent<TMP_InputField>().MoveToEndOfLine(false, false);
+        GetComponent<TMP_InputField>().ForceLabelUpdate();
+    }
+}
