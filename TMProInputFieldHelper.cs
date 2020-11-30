e95e33100492ac29257060a2b10a350555e08fe9
Xaverix <minesoft@wp.pl>
2020-11-30 21:15:42 +0100

    - InputField Helper cleanup

--- TMProInputFieldHelper.cs
+++ TMProInputFieldHelper.cs
@@ -7,18 +7,12 @@ using UnityEngine.EventSystems;
 public class TMProInputFieldHelper : MonoBehaviour
 {
     public void MoveToTextStart() {
-        //GetComponent<TMP_InputField>().textComponent.rectTransform.anchoredPosition = Vector2.zero;
-        //GetComponent<TMP_InputField>().stringPosition = 0;
-        //GetComponent<TMP_InputField>().caretPosition = 0;
-        //GetComponent<TMP_InputField>().ForceLabelUpdate();
         StartCoroutine(MoveToTextStartDelayed());
     }
     
     IEnumerator MoveToTextStartDelayed() {
         yield return new WaitForEndOfFrame();
-        //GetComponent<TMP_InputField>().Select();
         GetComponent<TMP_InputField>().MoveToStartOfLine(false, false);
-        //GetComponent<TMP_InputField>().ForceLabelUpdate();
     }
 
     public void MoveToTextEnd() {
